810faf035dd2e11d5c87aa2da40ee5eb041c9870
MukundPradhan <replyme.mmp@gmail.com>
2016-01-31 08:54:05 +0530

    Adding Class

---
+++ src/PrintPattern.java
@@ -0,0 +1,23 @@
+
+public class PrintPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int row,col;
+		
+		for(row=1; row<=5;row++){
+			for(col=1;col<=row;col++){
+				System.out.print(col);
+			}
+			System.out.println("\n");
+		}
+		
+		for(row=5; row>=1;row--){
+			for(col=1;col<row;col++){
+				System.out.print(col);
+			}
+			System.out.println("\n");
+		}
+	}
+
+}
